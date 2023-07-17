2c8f989efca70ab60d722e248145128158758dd6
Iain Taylor <iain.taylor.94@gmail.com>
2023-07-17 14:53:37 +0000

    completed program

--- .ccls-cache/@home@runner@Ch7Q3-array-of-counters-version-2/main.c
+++ .ccls-cache/@home@runner@Ch7Q3-array-of-counters-version-2/main.c
@@ -1,6 +1,47 @@
+// This program has a serious fault see readme.md for more details
+
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void) {
-  printf("Hello World\n");
+
+  int ratingCounters[10], i, response;
+  
+  // Get number of responses
+  int responseNumbers;
+  printf ("Enter the number of responses:  ");
+  scanf ("%d", &responseNumbers);
+
+  
+  // Set rating counter to zero
+  for (i = 1; i <= 10; i++)
+    ratingCounters[i] = 0;
+
+  
+  // Get responses
+  printf ("Enter your responses\n");
+
+  i = 1;
+  while (true) {
+    scanf ("%i", &response);
+
+    if (response < 1) 
+      printf ("Bad response: %i\n", response);
+    else
+      ++ratingCounters[response];
+
+    if (response == 999)
+      break;
+    
+    i++;
+  }
+
+  // Print the numbers of each response
+  printf ("\n\nRating Number of Responses\n");
+  printf ("------ -------------------\n");
+
+  for (i = 1; i <= 10; i++) 
+    printf ("%4i%22i\n", i, ratingCounters[i]);
+  
   return 0;
 }
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Programming in C
+## Chapter 7
+### Question 3
+
+This problem is from chapter 7 of the textbook, it is to modify program 7.2[^1] from the textbook to accept any number of responses.
+
+You may wish to run the program and compare its output.
+
+This programhas a serious error: when running the code the output is correct, however there is also an error message (stack smashing detected). I am unsure of the cause of this error, however since it is outside the scope of this exercise and the fact that the rest of the output is correct I am going to proceed to the next exercise in the textbook.
+
+[^1]: Programming in C (Third edition) By Stephen G. Kochan - Page 101 - Program 7.2
